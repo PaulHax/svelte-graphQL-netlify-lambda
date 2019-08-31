35cbaf70d9996b86d693c54bceb13bd140c06b5e
Paul <gladsomebeast@gmail.com>
2019-08-31 13:02:30 -0700

    message scrolling and sizing better

--- client-src/App.svelte
+++ client-src/App.svelte
@@ -1,10 +1,5 @@
 <script>
 	import HeadChat from './HeadChat.svelte';
-	import { myName } from './data';
-
-	// fetch("/.netlify/functions/hello-world")
-  //     .then(response => response.json())
-  //     .then(json => console.log(json))
 </script>
 
 <style>
--- client-src/MessageList.svelte
+++ client-src/MessageList.svelte
@@ -11,55 +11,78 @@
 	import { onMount, beforeUpdate, afterUpdate, tick } from 'svelte';
 	import { restore, query } from 'svelte-apollo';
 	import { myName } from './data';
-  
-  let activeMessageView;
+	
+	const STICKY_SCROLL_SLOP_PIXELS = 20;
+	
+	let activeMessageView;
+	
+	function checkAutoScroll() {
+		autoscroll = activeMessageView && 
+			(activeMessageView.offsetHeight + activeMessageView.scrollTop) 
+			> (activeMessageView.scrollHeight - STICKY_SCROLL_SLOP_PIXELS);
+	}
+
+	function maybeScrollToBottom() {
+		if (autoscroll) activeMessageView.scrollTo(0, activeMessageView.scrollHeight);
+	}
   
 	let autoscroll;
 	beforeUpdate(() => {
-		autoscroll = activeMessageView && (activeMessageView.offsetHeight + activeMessageView.scrollTop) > (activeMessageView.scrollHeight - 20);
+		checkAutoScroll();
 	});
 
 	afterUpdate(async () => {
 		await tick(); //seems we must wait for size of scrollHeight to fill in with all messages
-		if (autoscroll) activeMessageView.scrollTo(0, activeMessageView.scrollHeight);
+		maybeScrollToBottom();
 	});
 
+	function onResize(event) {		
+		maybeScrollToBottom();
+	}
+
+	function onMsgListScroll(event) {
+		checkAutoScroll();
+	}
+
 	onMount(() => {
+		//scroll to bottom
 		activeMessageView.scrollTo(0, activeMessageView.scrollHeight);
 	});
 
 	export let cache;
 	restore(client, MESSAGES, cache.data);
 	const messages = query(client, { query: MESSAGES });
-	$: console.log($messages);
+	//$: console.log($messages);
 </script>
 
+<svelte:window on:resize={onResize}/>
 
-	<ol class="messages" bind:this={activeMessageView}>
-		{#await $messages}
-			<li>Loading...</li>
-		{:then result}
-			{#each result.data.messages.data as message (message._id)}
-				<li class={message.author === $myName ? 'me': ''}>
-					<span>{message.author} : {message.text}</span>
-				</li>
-			{:else}
-				<li>No messages yet.</li>
-			{/each}
-		{:catch error}
-			<li>Error loading messages: {error}</li>
-		{/await}
-	</ol>
+<ol class="messages" bind:this={activeMessageView} on:scroll={onMsgListScroll}>
+	{#await $messages}
+		<li>Loading...</li>
+	{:then result}
+		{#each result.data.messages.data as message (message._id)}
+			<li class={message.author === $myName ? 'me': ''}>
+				<span>{message.author} : {message.text}</span>
+			</li>
+		{:else}
+			<li>No messages yet.</li>
+		{/each}
+	{:catch error}
+		<li>Error loading messages: {error}</li>
+	{/await}
+</ol>
 
 <style>
 	.messages {
-		flex-grow: 1;
+		/* flex-grow: 1; */
 
 		overflow-y: scroll;
+		/* flex-direction: column-reverse; */
 
 		padding: 0;
 		margin: 2px 0;
-		list-style-type: none;
+		/* list-style-type: none; */
 
 		/* max-height: 300px; */
 	}
@@ -75,7 +98,7 @@
 
 	.messages li span {
 		display: inline-block;
-		padding: .5em .5em .3em .5em;
+		padding: .5em .5em .5em .5em;
 		border-radius: 1em 1em 1em 0;
 		background: hsl(0, 0%, 98%);
     color: #000;
